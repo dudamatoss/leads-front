aab9b2ec14fa3f35b2c93c1566f3b3ad4ff9f1a5
dudamatoss <164113930+dudamatoss@users.noreply.github.com>
2025-08-14 08:53:12 -0300

    refactory in leads header

--- src/components/LeadsRow/LeadsHeader.tsx
+++ src/components/LeadsRow/LeadsHeader.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 export function LeadsHeader({ showParceiro = true }: Props) {
     const gridTemplate = showParceiro
-    ? "grid-cols-[1.5fr_1.1fr_1.3fr_1.2fr_1.2fr_1fr_1fr]"
-    : "grid-cols-[1.5fr_1.5fr_1.3fr_1.2fr_1fr_1fr]";
+    ? "grid-cols-[1.4fr_1.2fr_1.5fr_1.3fr_1.3fr_0.7fr_1fr]"
+    : "grid-cols-[1.5fr_1.5fr_1.7fr_1.4fr_1.4fr_0.5fr]";
     return (
         <div
-            className={`grid ${gridTemplate} w-full items-center justify-items-start gap-4 rounded-md bg-muted px-10 py-3 text-sm font-medium text-muted-foreground`}
+            className={`grid ${gridTemplate} w-full items-center justify-items-start gap-4 rounded-md bg-muted px-14 py-3 text-sm font-medium text-muted-foreground`}
         >
             {/*Contato*/}
             <div className="flex items-center gap-2 text-orange-500">
