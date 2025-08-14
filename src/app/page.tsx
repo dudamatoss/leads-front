9d68b2fe398d5ddd2a0946e44eb454d5fb04df16
dudamatoss <164113930+dudamatoss@users.noreply.github.com>
2025-08-14 15:30:38 -0300

    removing pagination in page not found leads and leads error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import {useEffect, useState, useCallback} from "react";
-import { LeadRow } from "@/components/LeadsRow/LeadRow";
-import { LeadCard } from "@/components/Cards/LeadCard";
-import { StatusFilter } from "@/components/Filters/StatusFilter";
-import { SearchFilter } from "@/components/Filters/SeaechFilter";
-import { OriginFilter, TypesFilter } from "@/components/Filters/DropDownFilter";
-import { LeadsHeader } from "@/components/LeadsRow/LeadsHeader";
-import { getLeadsTotais } from "@/lib/services/get-leads";
-import { LeadsTotais } from "@/schemas/leads-schemas";
-import { Handshake, UserCheck, Users2 } from "lucide-react";
-import { PaginationControls } from "@/components/Pagination/Pagination";
-import { NoLeadsFound } from "@/components/LeadsRow/NoticeLeads/NoLeadsFound";
-import { LeadsError } from "@/components/LeadsRow/NoticeLeads/LeadsError";
-import { LeadsLoading } from "@/components/LeadsRow/LeadsLoading";
-import { useLeadsPolling } from "@/hooks/use-leads";
+import {LeadRow} from "@/components/LeadsRow/LeadRow";
+import {LeadCard} from "@/components/Cards/LeadCard";
+import {StatusFilter} from "@/components/Filters/StatusFilter";
+import {SearchFilter} from "@/components/Filters/SeaechFilter";
+import {OriginFilter, TypesFilter} from "@/components/Filters/DropDownFilter";
+import {LeadsHeader} from "@/components/LeadsRow/LeadsHeader";
+import {getLeadsTotais} from "@/lib/services/get-leads";
+import {LeadsTotais} from "@/schemas/leads-schemas";
+import {Handshake, UserCheck, Users2} from "lucide-react";
+import {PaginationControls} from "@/components/Pagination/Pagination";
+import {NoLeadsFound} from "@/components/LeadsRow/NoticeLeads/NoLeadsFound";
+import {LeadsError} from "@/components/LeadsRow/NoticeLeads/LeadsError";
+import {LeadsLoading} from "@/components/LeadsRow/LeadsLoading";
+import {useLeadsPolling} from "@/hooks/use-leads";
 import {AnimationCards} from "@/components/Cards/AnimationCards";
 
 const ITEMS_FOR_PAGE = 8;
@@ -22,14 +22,14 @@ const ITEMS_FOR_PAGE = 8;
 export default function Home() {
     const [statusFilter, setStatusFilter] = useState<"ativos" | "concluidos">("ativos");
     const [typeFilter, setTypeFilter] = useState<"revenda" | "utilizacao" | "todos">("revenda");
-    const [originFilter, setOriginFilter] = useState<"Instagram"| "Facebook" | "Google" | "todos">("todos");
+    const [originFilter, setOriginFilter] = useState<"Instagram" | "Facebook" | "Google" | "todos">("todos");
     const [totais, setTotais] = useState<LeadsTotais | null>(null);
     const [page, setPage] = useState(1);
     const [busca, setBusca] = useState("");
     const [delayBusca, setDelayBusca] = useState("");
     const [totaisError, setTotaisError] = useState(false);
 
-    const { leads, loading, error: leadsError, refetch, totalPages  } = useLeadsPolling({
+    const {leads, loading, error: leadsError, refetch, totalPages} = useLeadsPolling({
         page,
         limit: ITEMS_FOR_PAGE,
         status: statusFilter === "ativos" ? "ativo" : "concluido",
@@ -40,7 +40,7 @@ export default function Home() {
     const error = leadsError || totaisError;
 
     useEffect(() => {
-        const handler = setTimeout(() => (setDelayBusca(busca) ), 200);
+        const handler = setTimeout(() => (setDelayBusca(busca)), 200);
         return () => clearTimeout(handler);
     }, [busca]);
 
@@ -68,7 +68,7 @@ export default function Home() {
     }, [statusFilter, typeFilter, originFilter, delayBusca]);
 
     useEffect(() => {
-        if(leadsError || totaisError) return ;
+        if (leadsError || totaisError) return;
         fetchTotais();
         const id = setInterval(fetchTotais, 3000);
         return () => clearInterval(id);
@@ -76,7 +76,7 @@ export default function Home() {
 
     useEffect(() => {
         setPage(1);
-    }, [statusFilter, typeFilter, originFilter,delayBusca]);
+    }, [statusFilter, typeFilter, originFilter, delayBusca]);
 
 
     const handleUpdateLead = () => {
@@ -112,33 +112,33 @@ export default function Home() {
                 <div className="w-full mt-10 mx-auto rounded-xl border shadow-sm bg-white sm:col-span-2 lg:col-span-3">
                     {/* Filtro de status */}
                     <div className="p-6">
-                        <StatusFilter value={statusFilter} onChange={setStatusFilter} />
+                        <StatusFilter value={statusFilter} onChange={setStatusFilter}/>
                     </div>
                     {/* Filtros */}
                     <div className="flex flex-wrap gap-4 p-6">
                         <SearchFilter placeholder="Buscar por nome..." value={busca} onChange={setBusca}/>
-                        <OriginFilter value={originFilter} onChange={setOriginFilter} />
-                        <TypesFilter value={typeFilter} onChange ={setTypeFilter} />
+                        <OriginFilter value={originFilter} onChange={setOriginFilter}/>
+                        <TypesFilter value={typeFilter} onChange={setTypeFilter}/>
                     </div>
                     {/* Título e subtítulo */}
                     <div className="pl-6 pt-6 pb-6">
                         <h2 className="pb-1 text-2xl font-bold">
-                            <AnimationCards value={totais?.totalGeral ?? leads.length} /> Leads
+                            <AnimationCards value={totais?.totalGeral ?? leads.length}/> Leads
                         </h2>
                         <h3 className="text-gray-500 text-l">Todas as leads registradas</h3>
                     </div>
                     {/* Cabeçalho da tabela */}
                     <div className="p-6">
-                        <LeadsHeader showParceiro={typeFilter !== "revenda"} />
+                        <LeadsHeader showParceiro={typeFilter !== "revenda"}/>
                     </div>
                     {/* Lista de leads */}
                     <div className="flex flex-col gap-y-3 p-6 pt-1">
                         {loading ? (
-                            <LeadsLoading />
+                            <LeadsLoading/>
                         ) : error ? (
-                            <LeadsError />
+                            <LeadsError/>
                         ) : leads.length === 0 ? (
-                            <NoLeadsFound />
+                            <NoLeadsFound/>
                         ) : (
                             leads.map((lead) => (
                                 <LeadRow
@@ -149,14 +149,17 @@ export default function Home() {
                                 />
                             ))
                         )}
-                        <div>
-                            <PaginationControls
-                            page={page}
-                            totalPages={totalPages}
-                            onPageChange={setPage}
-                            className="px-6 pb-6"
-                        />
-                        </div>
+                        {!loading && !error && leads.length > 0 && (
+
+                            <div>
+                                <PaginationControls
+                                    page={page}
+                                    totalPages={totalPages}
+                                    onPageChange={setPage}
+                                    className="px-6 pb-6"
+                                />
+                            </div>
+                        )}
 
                     </div>
                 </div>
