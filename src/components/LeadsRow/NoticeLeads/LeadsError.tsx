fb90b96e84f6a2da990af08d0f8c54f266d5c39a
dudamatoss <164113930+dudamatoss@users.noreply.github.com>
2025-08-15 09:10:49 -0300

    add button for reload of page in page of leads error

--- src/components/LeadsRow/NoticeLeads/LeadsError.tsx
+++ src/components/LeadsRow/NoticeLeads/LeadsError.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { AlertTriangle} from "lucide-react";
+import { AlertTriangle, RotateCcw} from "lucide-react";
+import {Button} from "@/components/ui/button";
 
 export function LeadsError() {
     return (
         <div className="flex flex-col items-center justify-center py-10 text-center text-red-600">
             <AlertTriangle className="h-12 w-12 mb-4 text-red-600" />
             <p className="text-lg font-medium">Ocorreu um erro ao carregar os leads</p>
-            <p className="text-sm">Alguém tem que ver isso ai...</p>
+            <div className="pt-4">
+            <Button className="text-red-600 bg-red-50 border-1 border-red-200 hover:text-red-700 hover:bg-red-100 "
+                    variant="ghost" onClick={() => window.location.reload()} ><RotateCcw /> Tentar novemente </Button>
+            </div>
+
         </div>
     )
 }
